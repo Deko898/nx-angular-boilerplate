8b11e894ac7af701d73f8578059b715cf1f4c66f
Dejan Obradovikj <Dejan.Obradovikj@endava.com>
2021-12-10 23:08:38 +0100

    initial commit

---
+++ apps/angular-boilerplate/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-angular-boilerplate-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-boilerplate';
+}
